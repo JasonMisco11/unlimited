7c4bad5bdd1d5a1f9fd63a60710fbaab051b567b
Jason Asamoah <JasonAs@stlghana.com>
2025-12-09 17:55:22 +0000

    Version1121

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Hero from "../features/Hero/Page"
-import Aboutt, { AboutUs, WobbleCardDemo } from "../features/WhyUs/Page";
 import WhyUs from "../features/WhyUs/Page";
 import FindUs from "@/features/FindUs/Page";
 export default function Home() {
