71b76f79ad484ba72d91232c120dda6bf9d16faa
Jason Asamoah <JasonAs@stlghana.com>
2025-12-10 07:56:51 +0000

    I

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "../features/Hero/Page"
-import WhyUs from "../features/WhyUs/Page";
+import WhyUs from "@/features/WhyUs/Page";
 import FindUs from "@/features/FindUs/Page";
 export default function Home() {
   return (
